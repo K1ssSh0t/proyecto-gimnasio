c2be7424673ddefd8ae4bc9d5ba34d8d7a3d5432
K1ssSh0t <angelomarmatias@gmail.com>
2023-04-08 20:29:02 -0600

    Agregado los modulos de ventas e inventarios

--- react_components/navBar.tsx
+++ react_components/navBar.tsx
@@ -2,46 +2,62 @@ import Link from "next/link";
 
 const NavigationMenu = () => {
   return (
-    <div className="flex flex-auto justify-between items-center bg-black shadow-lg py-4 px-6">
-      <div className="flex items-center">
-        <Link href="/" className="text-xl font-bold text-gray-800">
+    <div className="navbar bg-base-300">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52"
+          >
+            <li>
+              <Link href="/ventas">Ventas</Link>
+            </li>
+            <li>
+              <Link href="/inventario" className="justify-between">
+                Inventario
+              </Link>
+            </li>
+            <li>
+              <Link href="/trabajadores">Trabajadores</Link>
+            </li>
+          </ul>
+        </div>
+        <Link href="/" className="btn btn-ghost normal-case text-xl">
           Inicio
         </Link>
       </div>
-      <ul className="flex items-center">
-        <li>
-          <Link
-            href="/clientes"
-            className="text-gray-700 hover:text-gray-900 px-4 py-2 rounded-md"
-          >
-            Clientes
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/inventario"
-            className="text-gray-700 hover:text-gray-900 px-4 py-2 rounded-md"
-          >
-            Inventario
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/trabajadores"
-            className="text-gray-700 hover:text-gray-900 px-4 py-2 rounded-md"
-          >
-            Trabajadores
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/ventas"
-            className="text-gray-700 hover:text-gray-900 px-4 py-2 rounded-md"
-          >
-            Ventas
-          </Link>
-        </li>
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li className="">
+            <Link href="/ventas">Ventas</Link>
+          </li>
+          <li>
+            <Link href="/inventario" className="justify-between">
+              Inventario
+            </Link>
+          </li>
+          <li className=" ">
+            <Link href="/trabajadores">Trabajadores</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end"></div>
     </div>
   );
 };
