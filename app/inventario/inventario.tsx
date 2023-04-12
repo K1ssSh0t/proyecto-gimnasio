58a78b672ccfa8ee2e507169e730396e2aca1da6
K1ssSh0t <angelomarmatias@gmail.com>
2023-04-11 19:40:06 -0600

    Agregado funcionalidad a agregar un nuevo producto

--- app/inventario/inventario.tsx
+++ app/inventario/inventario.tsx
@@ -12,6 +12,20 @@ function InventoryModule({ listaProductos }: { listaProductos: Producto[] }) {
 
   const [productos, setProductos] = useState(listaProductos);
 
+  const [productoInicial, setProductoInicial] =
+    useState<Producto["inventario_incial"]>();
+
+  const [productoCaducidad, setProductoCaducidad] =
+    useState<Producto["feche_caducidad"]>();
+
+  const [productoCosto, setProductoCosto] = useState<Producto["costo"]>();
+
+  const [productoPrecio, setProductoPrcio] =
+    useState<Producto["precio_venta"]>();
+
+  const [productoActual, setProductoActual] =
+    useState<Producto["inventario_actual"]>();
+
   useEffect(() => {
     setProductos(productos);
   }, [productos]);
@@ -54,120 +68,141 @@ function InventoryModule({ listaProductos }: { listaProductos: Producto[] }) {
 
   /**                   */
 
+  const handleSummit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const { error } = await supabase.from("producto").insert({
+      costo: productoCosto,
+      precio_venta: productoPrecio,
+      feche_caducidad: productoCaducidad,
+      inventario_incial: productoInicial,
+      inventario_actual: productoActual,
+    });
+
+    console.log(error);
+  };
+
   return (
-    <div className="p-6">
+    <div className="p-6 ">
       <h1 className="text-3xl font-bold mb-6">Inventory Management</h1>
-      <form
-      //onSubmit={}
-      >
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 font-bold mb-2"
-            htmlFor="product-name"
-          >
-            Product Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="product-name"
-            type="text"
-            name="name"
-            // value={newProduct.name}
-            // onChange={handleInputChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 font-bold mb-2"
-            htmlFor="product-description"
-          >
-            Product Description
-          </label>
-          <textarea
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="product-description"
-            name="description"
-            ///   value={newProduct.description}
-            //  onChange={handleInputChange}
-          ></textarea>
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 font-bold mb-2"
-            htmlFor="product-price"
-          >
-            Product Price
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="product-price"
-            type="number"
-            step="0.01"
-            name="price"
-            // value={newProduct.price}
-            //  onChange={handleInputChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 font-bold mb-2"
-            htmlFor="product-quantity"
-          >
-            Product Quantity
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="product-quantity"
-            type="number"
-            name="quantity"
-            //   value={newProduct.quantity}
-            //  onChange={handleInputChange}
-          />
-        </div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
+      <div className=" flex justify-center ">
+        <form
+          onSubmit={handleSummit}
+          className=" lg:flex lg:flex-col w-full lg:max-w-3xl "
         >
-          Add Product
-        </button>
-      </form>
+          <div className="mb-4 w-full">
+            <label className="block  font-bold mb-2" htmlFor="product-name">
+              Producto Inicial
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="product-name"
+              type="number"
+              name="name"
+              value={productoInicial}
+              onChange={(event) => setProductoInicial(event.target.value)}
+            />
+          </div>
+          <div className="mb-4 w-full">
+            <label
+              className="block  font-bold mb-2"
+              htmlFor="product-description"
+            >
+              Product caducidad
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              id="product-description"
+              name="description"
+              type="date"
+              value={productoCaducidad}
+              onChange={(event) => setProductoCaducidad(event.target.value)}
+            ></input>
+          </div>
+          <div className="mb-4 w-full">
+            <label className="block  font-bold mb-2" htmlFor="product-price">
+              Product Costo
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              id="product-price"
+              type="number"
+              step="0.01"
+              name="price"
+              value={productoCosto}
+              onChange={(event) => setProductoCosto(event.target.value)}
+            />
+          </div>
+          <div className="mb-4 w-full">
+            <label className="block  font-bold mb-2" htmlFor="product-price">
+              Product Price
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              id="product-price"
+              type="number"
+              step="0.01"
+              name="price"
+              value={productoPrecio}
+              onChange={(event) => setProductoPrcio(event.target.value)}
+            />
+          </div>
+          <div className="mb-4 w-full">
+            <label className="block font-bold mb-2" htmlFor="product-quantity">
+              Product Quantity
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="product-quantity"
+              type="number"
+              name="quantity"
+              value={productoActual}
+              onChange={(event) => setProductoActual(event.target.value)}
+            />
+          </div>
+          <div className=" flex justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline "
+              type="submit"
+            >
+              Add Product
+            </button>
+          </div>
+        </form>
+      </div>
       <h2 className="text-2xl font-bold mt-6 mb-2">Inventory List</h2>
-      <table className="table-auto w-full">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">#</th>
-            <th className="px-4 py-2">Product ID</th>
-            <th className="px-4 py-2">Costo</th>
-            <th className="px-4 py-2">Precio Venta</th>
-            <th className="px-4 py-2">Inventario Actual</th>
-            <th className="px-4 py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {productos.map((producto, index) => (
-            <tr key={index}>
-              <td className="border px-4 py-2">{index + 1}</td>
-              <td className="border px-4 py-2">{producto.id}</td>
-              <td className="border px-4 py-2">{producto.costo}</td>
-              <td className="border px-4 py-2">{producto.precio_venta}</td>
-              <td className="border px-4 py-2">
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="number"
-                  name="quantity"
-                  // value={product.quantity}
-                  // onChange={(e) => handleInventoryUpdate(index, e.target.value)}
-                />
-              </td>
-              <td className="border px-4 py-2">
-                <button className="text-red-500 hover:text-red-700">
-                  Delete
-                </button>
-              </td>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product ID</th>
+              <th>Costo</th>
+              <th>Precio Venta</th>
+              <th>Inventario Actual</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {productos.map((producto, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{producto.id}</td>
+                <td>{producto.costo}</td>
+                <td>{producto.precio_venta}</td>
+                <td>{producto.inventario_actual}</td>
+                <td className="">
+                  <button className="text-red-500 hover:text-red-700">
+                    Delete
+                  </button>
+                  <button className=" text-red-500 hover:text-red-700 pl-3">
+                    Actualizar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
