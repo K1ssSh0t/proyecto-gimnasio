5140fadab41d91897c5d2b82a1842055482cca78
K1ssSh0t <angelomarmatias@gmail.com>
2023-06-07 11:01:40 -0600

    Agregado la vigencia

--- app/clientes/miMembresia.tsx
+++ app/clientes/miMembresia.tsx
@@ -4,6 +4,12 @@ import { Database } from "@/types/supabase";
 type Membresia = Database["public"]["Tables"]["membresia"]["Row"];
 
 export function MiMembresia({ membresia }: { membresia: Membresia }) {
+  const fecha = new Date(membresia.fecha_activacion as string);
+
+  const fecha_nueva = fecha.setMonth(fecha.getMonth() + 1);
+
+  const fecha_nueva_string = new Date(fecha_nueva).toJSON().slice(0, 10);
+
   return (
     <div className=" flex justify-center p-4">
       <div className="card w-4/5 bg-base-300 shadow-xl">
@@ -21,7 +27,9 @@ export function MiMembresia({ membresia }: { membresia: Membresia }) {
             <div className="stat">
               <div className="stat-title">Fecha de Activación</div>
               <div className="stat-value">{membresia.fecha_activacion}</div>
-              <div className="stat-desc">Vigente hasta ...</div>
+              <div className="stat-desc">
+                Vigente hasta {fecha_nueva_string}
+              </div>
             </div>
 
             <div className="stat">
